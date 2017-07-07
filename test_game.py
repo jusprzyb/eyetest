0decff270d9f85e3bfdf4a8a48c7474ab1e42701
jusprzyb <yamano991>
2017-07-07 14:29:20 +0200

    new

--- test_game.py
+++ test_game.py
@@ -1,9 +1,18 @@
 from browser import get_driver
+from collections import namedtuple
 from play import EyeGamePage
+import pytest
 
-def test_kret_lvl():
-    driver = get_driver()
+
+Level = namedtuple("Level", ['name', 'value'])
+
+ROBOT = Level(name='robot', value=30)
+JASTRZAB = Level(name='jastrzab', value=25)
+
+@pytest.mark.parametrize("level", [JASTRZAB, ROBOT])
+
+def test_lvl(driver, lvl):
     eye_game = EyeGamePage(driver)
     eye_game.load()
-    eye_game.get_to_robot()
+    eye_game.get_to_lvl(level=lvl)
     eye_game.check_robot_lvl_reached()
